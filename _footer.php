c80270150307d0dfdcb476f48172a16307a8040e
St4mpe <hampusekmansundin@gmail.com>
2026-02-10 15:12:33 +0100

    Starting

---
+++ _footer.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="footerStyle.css">
+<footer>
+    <section>
+        &copy The last of us crew
+    </section>
+</footer>
---
+++ _header.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="headerStyle.css">
+<header>
+    <h1>Last Of Us</h1>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="#">About</a>
+        <a href="#">Merch</a>
+        <a href="#">Contact</a>
+    </nav>
+</header>
