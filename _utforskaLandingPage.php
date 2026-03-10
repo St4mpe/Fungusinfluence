14c175383056b967a19d2cfec30246b21740bca0
St4mpe <hampusekmansundin@gmail.com>
2026-03-10 14:49:40 +0100

    1

--- _utforskaLandingPage.php
+++ _utforskaLandingPage.php
@@ -10,20 +10,31 @@
     <?php require_once("_header.php");?>
     <section class="main">
         <nav>
-            <a href="_utforskaLandingPage.php">Spelet</a>
+            <a class="bas" href="_utforskaLandingPage.php">Spelet</a>
             <a href="#">Tv-Serien</a>
             <a href="#">Karaktärerna</a>
         </nav>
-        <section class="displayVartDuÄr">
-            <h1>Spelet</h1>
-        </section>
         <section>
             <section class="firstPicture">
-                <section>
+                 <section>
                     <h2>Början på resan</h2>
                     <p>The Last of Us följer Joel och Ellie genom ett USA som har kollapsat efter <br> ett svamputbrott som förvandlat människor till farliga infekterade. Det som börjar som ett <br>uppdrag att eskortera Ellie genom landet utvecklas snabbt till en <br>resa fylld av faror, svåra val och oväntade band. Spelet blandar överlevnad, smygande och action <br> med en stark berättelse som driver spelaren framåt.</p>
                 </section>
                 <img src="https://m.media-amazon.com/images/M/MV5BMTE2MmQ3OTctZmExNi00ZTYxLTgzMGYtOTdlYjFjNzIxNWMxXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" alt="Pic of the last of us original copy">
+            </section>
+            <section class="secondPicture">
+                <img src="https://tpucdn.com/review/the-last-of-us-benchmark-test-performance-analysis/images/title.jpg" alt="Pic of the last of us original copy">
+                <section>
+                    <h2>En värld som tagits tillbaka av naturen</h2>
+                    <p>Miljöerna i spelet visar en värld där naturen långsamt har tagit tillbaka städer, <br> vägar och byggnader. Övergivna hus, sönderfallande städer och tysta skogar skapar en stämning <br> som är både vacker och sorglig. Den här kontrasten mellan <br> förfall och natur gör världen levande och bidrar starkt till spelets unika atmosfär.</p>
+                </section>
+            </section>
+            <section class="thirdPicture">
+                 <section>
+                    <h2>Karaktärer som känns verkliga</h2>
+                    <p>En av anledningarna till att så många älskar The Last of Us är de starka karaktärerna. <br> Joel och Ellie utvecklar en relation som förändras genom hela resan, och spelaren får uppleva <br> både deras konflikter och deras tillit till varandra. Dialogen, <br> skådespelet och de små ögonblicken mellan actionsekvenserna gör att <br>berättelsen känns personlig och äkta.</p>
+                </section>
+                <img src="https://cdn.mos.cms.futurecdn.net/LpdBuHarLswxF8xz9kvTNX.jpg" alt="Pic of the last of us original copy">
             </section>
         </section>
     </section>
