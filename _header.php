1857755b2495b881ee15aa33a0e15e2b42300196
St4mpe <hampusekmansundin@gmail.com>
2026-03-10 14:02:15 +0100

    1

--- _header.php
+++ _header.php
@@ -2,9 +2,8 @@
 <header>
     <img src="https://www.pngmart.com/files/22/The-Last-Of-Us-Logo-PNG-Pic.png" alt="">
     <nav>
-        <a href="index.php">Home</a>
-        <a href="#">About</a>
-        <a href="#">Merch</a>
-        <a href="#">Contact</a>
+        <a href="index.php">Hem</a>
+        <a href="_utforskaLandingPage.php">Utforska världen</a>
+        <a href="#">Shoppa merch</a>
     </nav>
 </header>
---
+++ _utforskaLandingPage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="_utforkaLandingPage.css">
+</head>
+<body>
+    <?php require_once("_header.php");?>
+    <section class="main">
+        <nav>
+            <a href="_utforskaLandingPage.php">Spelet</a>
+            <a href="#">Tv-Serien</a>
+            <a href="#">Karaktärerna</a>
+        </nav>
+        <section class="displayVartDuÄr">
+            <h1>Spelet</h1>
+        </section>
+        <section>
+            <section class="firstPicture">
+                <section>
+                    <h2>Början på resan</h2>
+                    <p>The Last of Us följer Joel och Ellie genom ett USA som har kollapsat efter <br> ett svamputbrott som förvandlat människor till farliga infekterade. Det som börjar som ett <br>uppdrag att eskortera Ellie genom landet utvecklas snabbt till en <br>resa fylld av faror, svåra val och oväntade band. Spelet blandar överlevnad, smygande och action <br> med en stark berättelse som driver spelaren framåt.</p>
+                </section>
+                <img src="https://m.media-amazon.com/images/M/MV5BMTE2MmQ3OTctZmExNi00ZTYxLTgzMGYtOTdlYjFjNzIxNWMxXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" alt="Pic of the last of us original copy">
+            </section>
+        </section>
+    </section>
+    <?php require_once("_footer.php");?>
+</body>
+</html>
