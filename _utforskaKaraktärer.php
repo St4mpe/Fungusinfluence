98c7068073ed191ad59fa7d8660afe7fc976086e
St4mpe <hampusekmansundin@gmail.com>
2026-03-24 15:10:55 +0100

    1

--- _utforskaKaraktärer.php
+++ _utforskaKaraktärer.php
@@ -16,23 +16,29 @@
         </nav>
         <section class="main">
             <section class="container">
-                <section>
+                <section class="grid-item">
                     <img src="https://static.wikia.nocookie.net/thelastofus/images/d/d1/Part_I_Joel_infobox.png/revision/latest/scale-to-width-down/1000?cb=20230215182931" alt="Joel">
+                    <section class="overlay-text">Joel</section>
                 </section>
-                <section>
+                <section class="grid-item">
                     <img src="https://static.wikia.nocookie.net/thelastofus/images/e/e3/Part_I_Ellie_infobox.png/revision/latest/scale-to-width-down/1000?cb=20230215221635" alt="Ellie">
+                    <section class="overlay-text">Ellie</section>
                 </section>
-                <section>
+                <section class="grid-item">
                     <img src="https://static.wikia.nocookie.net/thelastofus/images/d/d5/Part_I_Tess_infobox.png/revision/latest?cb=20230215183628" alt="Tess">
+                    <section class="overlay-text">Tess</section>
                 </section>
-                <section>
+                <section class="grid-item">
                     <img src="https://static.wikia.nocookie.net/thelastofus/images/d/d4/Part_I_Marlene_infobox.png/revision/latest?cb=20230222213251" alt="Marlene">
+                    <section class="overlay-text">Marlene</section>
                 </section>
-                <section>
+                <section class="grid-item">
                     <img src="https://static.wikia.nocookie.net/thelastofus/images/2/2f/Part_I_Tommy_infobox.png/revision/latest/scale-to-width-down/1000?cb=20230216035517" alt="Tommy">
+                    <section class="overlay-text">Tommy</section>
                 </section>
-                <section>
+                <section class="grid-item">
                     <img src="https://static.wikia.nocookie.net/thelastofus/images/0/0b/Sarah_Part_I_infobox.png/revision/latest/scale-to-width-down/1000?cb=20230815204341" alt="">
+                    <section class="overlay-text">Sarah</section>
                 </section>
             </section>
         </section>
