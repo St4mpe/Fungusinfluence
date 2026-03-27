d52ba92b261ae83535918eb4a90791a3fb482507
St4mpe <hampusekmansundin@gmail.com>
2026-03-27 10:20:26 +0100

    Karaktärerna

--- _utforskaKaraktärer.php
+++ _utforskaKaraktärer.php
@@ -25,23 +25,38 @@
                 </section>
                 <section class="grid-item">
                     <img src="https://static.wikia.nocookie.net/thelastofus/images/e/e3/Part_I_Ellie_infobox.png/revision/latest/scale-to-width-down/1000?cb=20230215221635" alt="Ellie">
-                    <section class="overlay-text">Ellie</section>
+                    <section class="overlay-text">
+                        <h2>Ellie</h2>
+                        <p>Brave, clever, and resilient, Ellie navigates a harsh, post-apocalyptic world. Her humor and determination contrast with her trauma, making her both a symbol of hope and emotional depth.</p>
+                    </section>
                 </section>
                 <section class="grid-item">
                     <img src="https://static.wikia.nocookie.net/thelastofus/images/d/d5/Part_I_Tess_infobox.png/revision/latest?cb=20230215183628" alt="Tess">
-                    <section class="overlay-text">Tess</section>
+                    <section class="overlay-text">
+                        <h2>Tess</h2>
+                        <p>Practical and unyielding, Tess is a seasoned smuggler who values loyalty and survival. Her tough exterior hides compassion, and she willingly takes risks for those she trusts.</p>
+                    </section>
                 </section>
                 <section class="grid-item">
                     <img src="https://static.wikia.nocookie.net/thelastofus/images/d/d4/Part_I_Marlene_infobox.png/revision/latest?cb=20230222213251" alt="Marlene">
-                    <section class="overlay-text">Marlene</section>
+                    <section class="overlay-text">
+                        <h2>Marlene</h2>
+                        <p>As the determined leader of the Fireflies, Marlene is idealistic and committed to humanity’s survival, willing to make difficult choices to secure a cure, even when morally ambiguous.</p>
+                    </section>
                 </section>
                 <section class="grid-item">
                     <img src="https://static.wikia.nocookie.net/thelastofus/images/2/2f/Part_I_Tommy_infobox.png/revision/latest/scale-to-width-down/1000?cb=20230216035517" alt="Tommy">
-                    <section class="overlay-text">Tommy</section>
+                    <section class="overlay-text">
+                        <h2>Tommy</h2>
+                        <p>Joel’s younger brother, principled and compassionate, seeks a better life amid chaos. He balances pragmatism with idealism, providing emotional stability and perspective in the brutal post-apocalyptic world.</p>
+                    </section>
                 </section>
                 <section class="grid-item">
                     <img src="https://static.wikia.nocookie.net/thelastofus/images/0/0b/Sarah_Part_I_infobox.png/revision/latest/scale-to-width-down/1000?cb=20230815204341" alt="">
-                    <section class="overlay-text">Sarah</section>
+                    <section class="overlay-text">
+                        <h2>Sarah</h2>
+                        <p>Joel’s daughter, innocent, loving, and full of hope. Her tragic fate profoundly impacts Joel, shaping his protective instincts, emotional journey, and the moral complexities of his actions.</p>
+                    </section>
                 </section>
             </section>
         </section>
