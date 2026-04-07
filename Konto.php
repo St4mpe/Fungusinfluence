334b39b831884100a6416e9ec1113a4d2d378146
St4mpe <hampusekmansundin@gmail.com>
2026-04-07 15:27:35 +0200

    §

--- Konto.php
+++ Konto.php
@@ -1,3 +1,23 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$db="fungus";
+$conn=mysqli_connect($host, $user, $pass, $db);
+
+if(isset($_POST['create']))
+{
+    $username=$_POST['name'];
+    $userpassword=$_POST['password'];
+    $useremail=$_POST['email'];
+    $userstad=$_POST['stad'];
+    $usergata=$_POST['gata'];
+    $userpostnummer=$POST['postnummer'];
+    $sql="INSERT INTO userinfo(user, pass, mail, stad, road, postnummer) VALUES ('$username','$userpassword', '$useremail', '$userstad', '$usergata', '$userpostnummer')";
+    $result=mysqli_query($conn,$sql);
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,13 +31,13 @@
     <div class="login-page">
         <div class="form">
             <form class="register-form" action="Konto.php" method="POST">
-                <input type="text" name="name" placeholder="Name"/>
-                <input type="password" name="password" placeholder="Password"/>
-                <input type="email" name="email" placeholder="Email Address"/>
+                <input type="text" name="name" placeholder="Namn"/>
+                <input type="password" name="password" placeholder="Lösenord"/>
+                <input type="email" name="email" placeholder="Mail Adress"/>
                 <input type="stad" name="stad" placeholder="Stad"/>
                 <input type="gata" name="gata" placeholder="Gata"/>
                 <input type="postnummer" name="postnummer" placeholder="Postnummer"/>
-                <input type="submit" value="Create Account"/>
+                <input type="submit" value="Create Account" name="create">
                 <p class="message">Already registered? <a href="#">Sign In</a></p>
             </form>
             <form class="login-form" action="Konto.php" method="POST">
