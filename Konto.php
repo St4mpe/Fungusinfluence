7aa6c644f9e0f0af8a253d54f38f9172a9334afa
St4mpe <hampusekmansundin@gmail.com>
2026-04-07 14:19:02 +0200

    login ruta

---
+++ Konto.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="kontoStyle.css">
+</head>
+<body>
+    <section class="main">
+        <section class="rutan">
+            <h1>Logga in</h1>
+            <form action="Konto.php">
+                <label for="användare">Användare</label><br>
+                <input type="text" id="användare" name="användare"><br><br>
+                <label for="lösen">Lösenord</label><br>
+                <input type="password" id="lösen" name="lösen"><br><br>
+                <input type="submit" value="Submit">
+            </form>
+        </section>
+    </section>
+</body>
+</html>
