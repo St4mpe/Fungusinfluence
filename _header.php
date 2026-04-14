10a97b7458d5afaaf1912eccaf61bbc28ed76766
St4mpe <hampusekmansundin@gmail.com>
2026-04-14 14:17:11 +0200

    1

--- _header.php
+++ _header.php
@@ -4,34 +4,37 @@ require_once("functions.php");
 <link rel="stylesheet" href="headerStyle.css">
 <header>
     <img src="https://www.pngmart.com/files/22/The-Last-Of-Us-Logo-PNG-Pic.png" alt="">
-    <nav>
-        <a href="index.php">Hem</a>
-        <a href="_utforskaLandingPage.php">Utforska världen</a>
-        <a href="webbshop.php">Shoppa merch</a>
-        <?php 
-        
-        if (isset($_SESSION['userLoggedIn']))
-        {?>
+    <section class="navbar">
+        <nav>
+            <a href="index.php">Hem</a>
+            <a href="_utforskaLandingPage.php">Utforska världen</a>
+            <a href="webbshop.php">Shoppa merch</a>
             <?php 
-            if ($_SESSION['userLoggedIn'] == 1)
+            
+            if (isset($_SESSION['userLoggedIn']))
             {?>
-                <a href="MyPage.php">Konto</a>
-            <?php
-            }
+                <?php 
+                if ($_SESSION['userLoggedIn'] == 1)
+                {?>
+                    <a href="MyPage.php">Konto</a>
+                    <a href="kundvagn.php">Kundvagn</a>
+                <?php
+                }
+                else
+                {?>
+                    <a href="Konto.php">Logga in</a>
+                    <?php
+                }
+                ?>
+            <?php  
+            } 
             else
             {?>
-                <a href="Konto.php">Logga in</a>
+                <a href="Konto.php">Logga in</a>    
                 <?php
             }
             ?>
-        <?php  
-        } 
-        else
-        {?>
-            <a href="Konto.php">Logga in</a>    
-            <?php
-        }
-        ?>
-
-    </nav>
+        </nav>
+        
+    </section>
 </header>
